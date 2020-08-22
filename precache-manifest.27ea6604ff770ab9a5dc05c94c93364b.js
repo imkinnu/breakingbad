247f1ca9ac5682e7750e6f273d28c676f1186d68
Kiran Tirumlasetti <50947441+imkinnu@users.noreply.github.com>
2020-08-22 09:26:05 +0530

    Updates

---
+++ precache-manifest.27ea6604ff770ab9a5dc05c94c93364b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f30a1211bcc724fee8c221aa36718d0",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "fef3be704abe66de2ad6",
+    "url": "/breakingbad/static/css/main.9e606cda.chunk.css"
+  },
+  {
+    "revision": "54db7f5d435159edfe92",
+    "url": "/breakingbad/static/js/2.e6319f52.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/breakingbad/static/js/2.e6319f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef3be704abe66de2ad6",
+    "url": "/breakingbad/static/js/main.e7d7a1d2.chunk.js"
+  },
+  {
+    "revision": "ea2d3b5d3b339cb26b3a",
+    "url": "/breakingbad/static/js/runtime-main.e09d8a2d.js"
+  },
+  {
+    "revision": "62fa13c8d6a7597eef48bdd7e57de4a3",
+    "url": "/breakingbad/static/media/Heart Breaking Bad.62fa13c8.otf"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingbad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingbad/static/media/logo.3795f66d.png"
+  }
+]);
